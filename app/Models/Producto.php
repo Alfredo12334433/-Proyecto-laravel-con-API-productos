0c4879ea13f6cb0668dc9d8fd21076547fde82a8
Padilla Mancilla Alfredo <al222010674@gmail.com>
2021-11-22 15:44:27 -0600

    modelo de productos y validacion

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Producto extends Model
 {
     use HasFactory;
+
+    protected $table = 'productos';
+
+    protected $fillable = [
+                'producto'
+                , 'existencias'
+                , 'precio_unitario' 
+                , 'fotografia'  
+                , 'descripcion' 
+                , 'estatus' 
+                , 'tasa_iva' 
+    ];
+
+    //reglas de validacion
+    public static function reglas() {
+        return[
+            'producto' => 'required|max:45'
+            , 'existencias' => 'required|integer|min:0'
+            , 'precio_unitario' => 'required|numeric|min:0'
+            , 'fotografia' => 'required|max:45'
+            , 'descripcion' => 'required|max:1000'
+            , 'estatus' => 'required|in:Activo,Inactivo'
+            , 'tasa_iva' => 'required|numeric|min:0'
+        ];
+    }
 }
